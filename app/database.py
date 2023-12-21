dbd690d86b783ca49166d628bdd190ebc4038d75
oizamadpedro <112671824+oizamadpedro@users.noreply.github.com>
2023-12-21 00:06:52 -0300

    add delete, getone product

--- app/database.py
+++ app/database.py
@@ -16,6 +16,14 @@ config = {
   'database': DB_NAME
 }
 
+def insDB(query, values):
+  db = mysql.connector.connect(**config)
+  cursor = db.cursor(dictionary=True)
+  cursor.execute(query, values)
+  db.commit()
+  cursor.close()
+  db.close()
+
 def selectAllProducts():
   db = mysql.connector.connect(**config)
   cursor = db.cursor(dictionary=True)
@@ -26,15 +34,23 @@ def selectAllProducts():
   return rows
 
 def insertProduct(product):
-    db = mysql.connector.connect(**config)
-    cursor = db.cursor(dictionary=True)
-    query = "INSERT INTO products (product, price, buy_date, rate_product, sell_date, quantity) VALUES (%s, %s, %s, %s, %s, %s)"
-    values = (product.product, product.price, product.buy_date, product.rate_product, product.sell_date, product.quantity)
-    cursor.execute(query, values)
-    db.commit()
-    cursor.close()
-    db.close()
-    
-    return product
+  query = "INSERT INTO products (product, price, buy_date, rate_product, sell_date, quantity) VALUES (%s, %s, %s, %s, %s, %s)"
+  values = (product.product, product.price, product.buy_date, product.rate_product, product.sell_date, product.quantity)
+  insDB(query, values)
+  return product
 
+def delete(product_id):
+  query = "delete from products where id=%s"
+  values = (product_id,)
+  insDB(query, values)
+  return True
 
+def selectOneProduct(product_id):
+  db = mysql.connector.connect(**config)
+  cursor = db.cursor(dictionary=True)
+  query = ("SELECT * FROM products where id=%s;")
+  values = (product_id,)
+  result = cursor.execute(query, values)
+  rows = cursor.fetchall()
+  db.close()
+  return rows
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from database import selectAllProducts, insertProduct
+from database import selectAllProducts, insertProduct, delete, selectOneProduct
 
 class Product(BaseModel):
     product: str
@@ -12,13 +12,22 @@ class Product(BaseModel):
 
 app = FastAPI()
 
-@app.get("/v1/getProducts")
+@app.get("/v1/products")
 async def getProducts():
     data = selectAllProducts()
     return {'data': data}
 
-@app.post("/v1/createProduct")
+@app.post("/v1/products")
 async def createProduct(product: Product):
     data = insertProduct(product)
     return {'data': data, 'message': 'created with succesful.'}
 
+@app.delete("/v1/products/{product_id}")
+async def deleteProduct(product_id):
+    data = delete(product_id)
+    return {'message': 'deleted with succesful.'}
+
+@app.get("/v1/products/{product_id}")
+async def getOneProduct(product_id):
+    data = selectOneProduct(product_id)
+    return {'data': data}
