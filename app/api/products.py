3a51c5b60bf041c3253c83f34096c9050cf70cc3
oizamadpedro <112671824+oizamadpedro@users.noreply.github.com>
2024-01-16 22:45:09 -0300

    product endpoint 100% test

--- app/api/products.py
+++ app/api/products.py
@@ -1,27 +1,30 @@
 from fastapi import APIRouter
 import json
-from fastapi.responses import JSONResponse
+from fastapi.responses import Response
 from services.ProductService import Products
 from base.baseModels import Product
 
 router = APIRouter()
 
+def payloadReturn(data):
+    return Response(content=json.dumps(data),  media_type="application/json", status_code=data['status'])
+
 @router.get("/")
 async def getProducts():
     data = Products.get()
-    return data
+    return payloadReturn(data)
 
 @router.post("/")
 async def createProduct(product: Product):
     data = Products.post(product)
-    return JSONResponse(status_code=data['status'], content=json.dumps(data))
+    return payloadReturn(data)
 
 @router.delete("/{product_id}")
 async def deleteProduct(product_id):
     data = Products.delete(product_id)
-    return JSONResponse(status_code=data['status'], content=json.dumps(data))
+    return payloadReturn(data)
 
 @router.get("/{product_id}")
 async def getOneProduct(product_id):
     data = Products.getById(product_id)
-    return JSONResponse(status_code=data['status'], content=json.dumps(data))
+    return payloadReturn(data)
--- app/tests/v1/test_products.py
+++ app/tests/v1/test_products.py
@@ -15,6 +15,7 @@ def created_product(payload):
     create_product_response = requests.post(ENDPOINT, json=payload)
     data = json.loads(create_product_response.json())
     created_product = data['data'][0]
+    assert created_product['product'] == payload['product']
     assert create_product_response.status_code == 201
     yield created_product  # A fixture retorna o produto criado
     # O código abaixo será executado após o teste, removendo o produto criado
@@ -23,7 +24,7 @@ def created_product(payload):
     print(delete_product_response.json())
     assert delete_product_response.status_code == 200
 
-def test_get_one_product(created_product):
+def test_getById(created_product):
     # Utiliza o produto criado para realizar verificações de get
     print(created_product['id'])
     response = requests.get(ENDPOINT + str(created_product['id']))
@@ -39,9 +40,24 @@ def test_falha_em_pegar_produto_nao_existente():
     assert data['status'] == 404
     assert data['error']['message'] == "product not found"
 
+def test_get_all_products(created_product):
+    response = requests.get(ENDPOINT)
+    data = json.loads(response.json())
+    assert data['data'][-1]['product'] == created_product['product']
+    assert data['status'] == 200
+    assert response.status_code == 200
+
+def test_fail_delete_product_not_exist():
+    response = requests.delete(ENDPOINT + "9128319082")
+    data = json.loads(response.json())
+    assert response.status_code == 404
+    assert "error" in data
+    assert data['status'] == 404
+    assert data['error']['message'] == "product not found"
+
 def test_get_products():
     response = requests.get(ENDPOINT, verify=False)
-    data = response.json()
+    data = json.loads(response.json())
     assert response.status_code == 200
     assert "data" in data
     assert len(data['data']) > 0
